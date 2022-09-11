ddb09724c7f96ea8dbf72fcb7f777a4d41e845a4
Titi <animasauntitiloluwa@gmail.com>
2022-09-11 11:23:32 -0700

    corrected trailing whitespace

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  * main - prints Programming is like building a multilingual puzzle
+ *
+ *   *
+ *
+ *    * Return: Always 0 (sucess)
+ *
+ *    **/
+
+
+
+int main(void)
+
+{
+
+		char string[] = "\"Programming is like building a multilingual puzzle";
+
+
+			puts(string);
+
+
+				return (0);
+
+}
