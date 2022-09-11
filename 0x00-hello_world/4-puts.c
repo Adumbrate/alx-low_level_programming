08d88292d8984c3f7628ab92c494d9d262d4cfac
Titi <animasauntitiloluwa@gmail.com>
2022-09-11 11:47:42 -0700

    refix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,14 @@
 
 
 /**
- 
    * main - prints Programming is like building a multilingual puzzle
    *
    * Return: Always 0 (sucess)
-   *
  **/
 
 
 int main(void)
 {
-
 		char string[] = "\"Programming is like building a multilingual puzzle";
 
 		puts(string);
