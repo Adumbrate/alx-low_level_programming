52815df87374da410cea603875b985583a51ece9
Titi <animasauntitiloluwa@gmail.com>
2022-09-12 14:50:17 -0700

    refix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
-
  * main - print all single digit numbers up to 9
-
  *
-
  * Return: Always 0 (sucess)
-
- */
-
-
+**/
 
 int main(void)
 
 {
 
 	int i = 0;
-
-
 
 	while (i < 10)
 
