d4c6a1a8e72a8906142a4efb4c9e7f7f7d01b2db
Titi <animasauntitiloluwa@gmail.com>
2022-09-12 20:09:35 -0700

    refix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,54 +8,28 @@
  * Return: Always 0 (sucess)
  **/
 
-
-
 int main(void)
-
 {
-
 	int i, j;
 
-
-
 	for (i = '0'; i <= '9'; i++)
-
 	{
-
 		for (j = i + 1; j <= '9'; j++)
-
 		{
-
 			putchar(i);
-
 			putchar(j);
 
-
-
 			if (i == '8' && j == '9')
-
 			{
-
 				continue;
-
 			}
 
-
-
 			putchar(',');
-
 			putchar(' ');
 
-
-
 		}
-
 	}
 
-
-
 	putchar('\n');
-
 	return (0);
-
 }
