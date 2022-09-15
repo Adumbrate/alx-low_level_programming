7aae3fa05668ba116a873db6e939015d3c1d9db5
Titi <animasauntitiloluwa@gmail.com>
2022-09-14 18:41:21 -0700

    edit alphabet file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing all the lowercase letters of the alphabet
+**/
+
+
+void print_alphabet(void)
+{
+
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+
+	{
+
+		_putchar(letters);
+
+	}
+
+	_putchar('\n');
+
+}
