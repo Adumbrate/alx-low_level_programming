185c31b5b3e4469ef2ffcc49f8692aec6deb9bd6
Titi <animasauntitiloluwa@gmail.com>
2022-09-15 07:42:23 -0700

    refix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,25 +5,15 @@
 **/
 
 void times_table(void)
-
 {
-
 	int i, j, product;
 
-
-
 	for (i = 0; i < 10; i++)
-
 	{
-
 		for (j = 0; j < 10; j++)
-
 		{
-
 			if (j == 0)
-
 			{
-
 				_putchar('0');
 				continue;
 			}
@@ -32,29 +22,17 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-
-
 			if (product >= 10)
-
 			{
-
 				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
-
 			}
-
 			else
 			{
-
 				_putchar(' ');
 				_putchar(product + '0');
-
 			}
-
 		}
-
 		_putchar('\n');
-
 	}
-
 }
