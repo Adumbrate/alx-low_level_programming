5f6f364bd67b4fdd7b952493d58f4e3895213a21
Titi <animasauntitiloluwa@gmail.com>
2022-09-15 10:42:56 -0700

    refix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,12 +4,11 @@
  * main - printing the first 98 fibonacci numbers
  *
  * Return: Always 0 (success)
- */
+**/
 
 int main(void)
 {
 	int count;
-
 	unsigned long fib1 = 0, fib2 = 1, sum;
 	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 	unsigned long half1, half2;
@@ -21,7 +20,6 @@ int main(void)
 
 		fib1 = fib2;
 		fib2 = sum;
-
 	}
 
 	fib1_half1 = fib1 / 10000000000;
@@ -31,13 +29,11 @@ int main(void)
 
 	for (count = 93; count < 99; count++)
 	{
-
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
 
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
-
 			half1 += 1;
 			half2 %= 10000000000;
 		}
@@ -46,16 +42,13 @@ int main(void)
 
 		if (count != 98)
 		{
-
 			printf(", ");
 		}
-
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-
 	printf("\n");
 	return (0);
 }
