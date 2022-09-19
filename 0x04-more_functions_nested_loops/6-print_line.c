6693adde16d9e604665ad26e7d62125c1227109e
Titi <animasauntitiloluwa@gmail.com>
2022-09-19 02:52:35 -0700

    refix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,25 +8,19 @@
 
 void print_line(int n)
 {
+	int ;
 
 	for (i = 0; i < n; i++)
 
 	{
-
 		if (n > 0)
 		{
-
 			_putchar('_');
-
 		}
 		else
-
 		{
-
 			_putchar('\n');
-
 		}
 	}
 	_putchar('\n');
-
 }
