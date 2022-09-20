5d82b4f58ead0ff1d0de2df8a9628727cea62238
Titi <animasauntitiloluwa@gmail.com>
2022-09-19 17:04:58 -0700

    refix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,13 @@
 #include "main.h"
-
 #include <string.h>
 
-
-
 /**
-
  * _strcpy - copy the contents of an array
-
  * @dest: address to copy to
-
  * @src: address to copy from
-
  *
-
  * Return: pointer to destination
-
- */
-
-
+**/
 
 char *_strcpy(char *dest, char *src)
 
