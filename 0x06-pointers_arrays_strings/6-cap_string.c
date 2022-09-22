9d4651ef5076adc0b7b437d355f47f35548476d2
Titi <animasauntitiloluwa@gmail.com>
2022-09-22 06:47:04 -0700

    added j initialization

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,57 +8,30 @@
 **/
 
 char *cap_string(char *s)
-
 {
-
 	int i = 0, j;
-
 	char a[] = " \t\n,;.!?\"(){}";
 
-
-
 	while (*(s + i))
-
 	{
-
 		if (s[i] >= 'a' && s[i] <= 'z')
-
 		{
-
 			if (i == 0)
-
 			{
-
 				s[i] = s[i] - 32;
-
 			}
-
 			else
-
 			{
-
 				for (j = 0; j <= 12; j++)
-
 				{
-
 					if (a[j] == *(s + i - 1))
-
 					{
-
 						s[i] -= 32;
-
 					}
-
 				}
-
 			}
-
 		}
-
 		i++;
-
 	}
-
 	return (s);
-
 }
