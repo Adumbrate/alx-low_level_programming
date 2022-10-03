688e4aca86056bcc87cd8906ea12acab8236dcd1
Titi <animasauntitiloluwa@gmail.com>
2022-10-02 20:00:36 -0700

    print arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments passes one after another
+ * @argc: argument count
+ * @argv: array passes
+ *
+ * Return: 0
+**/
+
+int main(int argc, char **argv)
+
+{
+
+	int i;
+
+
+
+	for (i = 0; i < argc; i++)
+
+	{
+
+		printf("%s\n", argv[i]);
+
+	}
+
+	return (0);
+
+}
