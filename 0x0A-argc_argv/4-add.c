a30ec30f64b54ed22cb3e4e8ef789d369dd7c955
Titi <animasauntitiloluwa@gmail.com>
2022-10-02 20:08:09 -0700

    quick fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,41 +10,20 @@
 **/
 
 int main(int argc, char *argv[])
-
 {
-
 	int i, sum;
-
-
-
 	sum = 0;
-
-
-
 	for (i = 0; i < argc; i++)
-
 	{
-
 		if (i > 0)
-
 		{
-
 			if (atoi(argv[i]) == 0)
-
 			{
-
 				return (printf("Error\n"), 1);
-
 			}
-
 			sum += atoi(argv[i]);
-
 		}
-
 	}
-
 	printf("%d\n", sum);
-
 	return (0);
-
 }
